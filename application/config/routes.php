928203f27b87f1775f993431fcedd1517d2372ec
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-23 01:17:46 +0300

    Example 8: A correction about the  routing rule.

--- application/config/routes.php
+++ application/config/routes.php
@@ -59,4 +59,4 @@ $route['translate_uri_dashes'] = TRUE;
 | -------------------------------------------------------------------------
 */
 $route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
-$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.?)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
+$route['api/example/users/(:num)(\.)([a-zA-Z0-9_-]+)(.*)'] = 'api/example/users/id/$1/format/$3$4'; // Example 8
