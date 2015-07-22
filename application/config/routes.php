5bee94f22a50638d82cf2144e3084e7aeffefb1e
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-23 00:08:12 +0300

    Adding a route rule for Example 4.

--- application/config/routes.php
+++ application/config/routes.php
@@ -51,6 +51,11 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 */
 $route['default_controller'] = 'welcome';
 $route['404_override'] = '';
-
-//$route['translate_uri_dashes'] = FALSE;
 $route['translate_uri_dashes'] = TRUE;
+
+/*
+| -------------------------------------------------------------------------
+| Sample REST API Routes
+| -------------------------------------------------------------------------
+*/
+$route['api/example/users/(:num)'] = 'api/example/users/id/$1'; // Example 4
