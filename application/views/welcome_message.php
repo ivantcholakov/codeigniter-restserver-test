31bd7792436be05fdcabd69f1df58513bd335d91
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-07-25 20:28:48 +0300

    Show the link to the local CodeIgniter's documentation if it is there.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -86,7 +86,9 @@ defined('BASEPATH') OR exit('No direct script access allowed');
         <p>The corresponding controller for this page is found at:</p>
         <code>application/controllers/Welcome.php</code>
 
-        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="user_guide/">User Guide</a>.</p>
+        <?php if (file_exists(FCPATH.'user_guide/index.html')) : ?>
+        <p>If you are exploring CodeIgniter for the very first time, you should start by reading the <a href="<?php echo base_url('user_guide/index.html'); ?>" target="_blank">User Guide</a>.</p>
+        <?php endif ?>
     </div>
 
     <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
