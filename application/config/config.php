bf2daadc8fcb3468c8a711ca2348155e7aca7974
Ivan Tcholakov <ivantcholakov@gmail.com>
2015-08-08 22:06:50 +0300

    Enabling Composer autoloader for PHP>=5.5, It is Guzzle 6 requirement.

--- application/config/config.php
+++ application/config/config.php
@@ -130,7 +130,7 @@ $config['subclass_prefix'] = 'MY_';
 | Note: This will NOT disable or override the CodeIgniter-specific
 |	autoloading (application/config/autoload.php)
 */
-$config['composer_autoload'] = TRUE;
+$config['composer_autoload'] = is_php('5.5');   // For Guzzle HTTP Client 6.
 
 /*
 |--------------------------------------------------------------------------
