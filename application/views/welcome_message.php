ef77b0bb6808b8757acfd03036eb379b7f425aa4
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-04-18 01:11:39 +0300

    Updates from the original project.

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -103,7 +103,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
     </div>
 
-    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>' . CI_VERSION . '</strong>' : '' ?></p>
+    <p class="footer">Page rendered in <strong>{elapsed_time}</strong> seconds. <?php echo  (ENVIRONMENT === 'development') ?  'CodeIgniter Version <strong>'.CI_VERSION.'</strong>' : '' ?></p>
 </div>
 
 </body>
