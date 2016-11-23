f8aec7019c040cf20fd03e662a5fd035fe8caa08
Ivan Tcholakov <ivantcholakov@gmail.com>
2016-11-23 22:51:38 +0200

    composer update Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- application/vendor/composer/autoload_static.php
+++ application/vendor/composer/autoload_static.php
@@ -7,8 +7,8 @@ namespace Composer\Autoload;
 class ComposerStaticInit54058c9d5fd9edc6c7a76b6c9c5c789d
 {
     public static $files = array (
-        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         'a0edc8309cc5e1d60e3047b5df6b7052' => __DIR__ . '/..' . '/guzzlehttp/psr7/src/functions_include.php',
+        'c964ee0ededf28c96ebd9db5099ef910' => __DIR__ . '/..' . '/guzzlehttp/promises/src/functions_include.php',
         '37a3dc5111fe8f707ab4c132ef1dbc62' => __DIR__ . '/..' . '/guzzlehttp/guzzle/src/functions_include.php',
     );
 
