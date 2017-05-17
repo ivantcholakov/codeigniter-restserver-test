7116ad1c6d393085eabe8d1cf99a6d2a846a7f5d
Ivan Tcholakov <ivantcholakov@gmail.com>
2017-05-17 13:28:57 +0300

    Adding two Composer packages: "roave/security-advisories" and "paragonie/random_compat". Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

--- application/vendor/composer/autoload_files.php
+++ application/vendor/composer/autoload_files.php
@@ -11,4 +11,5 @@ return array(
     'c964ee0ededf28c96ebd9db5099ef910' => $vendorDir . '/guzzlehttp/promises/src/functions_include.php',
     '8cff32064859f4559445b89279f3199c' => $vendorDir . '/php-http/message/src/filters.php',
     '37a3dc5111fe8f707ab4c132ef1dbc62' => $vendorDir . '/guzzlehttp/guzzle/src/functions_include.php',
+    '5255c38a0faeba867671b61dfda6d864' => $vendorDir . '/paragonie/random_compat/lib/random.php',
 );
