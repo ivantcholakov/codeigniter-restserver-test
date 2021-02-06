6dcc8803476b345b1a97bcbfdce0ef935379429e
Ivan Tcholakov <ivantcholakov@gmail.com>
2021-02-06 03:20:12 +0200

    Finishing installation of the custom Email library. Signed-off-by:Ivan Tcholakov <ivantcholakov@gmail.com>

---
+++ application/helpers/MY_email_helper.php
@@ -0,0 +1,23 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
+// A place where you can move your custom helper functions,
+// that are to be loaded before the functions below.
+// If it is needed, create the corresponding file, insert
+// your source there and uncomment the following lines.
+//if (is_file(dirname(__FILE__).'/MY_email_helper_0.php')) {
+//    require_once dirname(__FILE__).'/MY_email_helper_0.php';
+//}
+
+// Instead of copying manually or through script in this directory,
+// let us just load here the provided by Composer file.
+if (is_file(VENDORPATH.'ivantcholakov/codeigniter-phpmailer/helpers/MY_email_helper.php')) {
+    require_once VENDORPATH.'ivantcholakov/codeigniter-phpmailer/helpers/MY_email_helper.php';
+}
+
+// A place where you can move your custom helper functions,
+// that are to be loaded after the functions above.
+// If it is needed, create the corresponding file, insert
+// your source there and uncomment the following lines.
+//if (is_file(dirname(__FILE__).'/MY_email_helper_2.php')) {
+//    require_once dirname(__FILE__).'/MY_email_helper_2.php';
+//}
---
+++ application/libraries/MY_Email.php
@@ -0,0 +1,5 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed.');
+
+// Instead of copying manually or through script in this directory,
+// let us just load here the provided by Composer file.
+require_once VENDORPATH.'ivantcholakov/codeigniter-phpmailer/libraries/MY_Email.php';
